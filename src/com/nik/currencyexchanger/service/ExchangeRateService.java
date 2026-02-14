eef82650bb45550b3b4c28b68b58a48678b96dd7
dab1231 <nikita11200701@gmail.com>
2026-02-14 23:57:02 +0400

    add getExchangeRate method and update imports

--- src/com/nik/currencyexchanger/service/ExchangeRateService.java
+++ src/com/nik/currencyexchanger/service/ExchangeRateService.java
@@ -3,6 +3,7 @@ package com.nik.currencyexchanger.service;
 import com.nik.currencyexchanger.dao.ExchangeRateDao;
 import com.nik.currencyexchanger.dto.ExchangeRateDto;
 import com.nik.currencyexchanger.entity.ExchangeRate;
+import com.nik.currencyexchanger.exception.ExchangeRateNotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,13 +18,23 @@ public class ExchangeRateService {
 
     }
 
-    public List<ExchangeRateDto> findAll(){
+    public List<ExchangeRateDto> getAllExchangeRates(){
         var exchangeRates = exchangeRateDao.findAll();
         List<ExchangeRateDto> exchangeRateDtos = new ArrayList<>();
         for(ExchangeRate exchangeRate : exchangeRates){
             exchangeRateDtos.add(buildDto(exchangeRate));
         }
         return exchangeRateDtos;
+    }
+
+    public ExchangeRateDto getExchangeRate(String baseCode, String targetCode){
+        var baseCurrencyDto = currencyService.findByCode(baseCode);
+        var targetCurrencyDto = currencyService.findByCode(targetCode);
+        var baseId = baseCurrencyDto.getId();
+        var targetId = targetCurrencyDto.getId();
+        var exchangeRateOptional = exchangeRateDao.findByCurrenciesId(baseId, targetId);
+        var exchangeRate = exchangeRateOptional.orElseThrow(() -> new ExchangeRateNotFoundException());
+        return buildDto(exchangeRate);
     }
 
     private ExchangeRateDto buildDto(ExchangeRate exchangeRate){
