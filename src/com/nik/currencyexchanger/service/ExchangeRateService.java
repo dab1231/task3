177cf1a96c3a8105bdb21a71d2ad16f5a7e50b19
dab1231 <nikita11200701@gmail.com>
2026-02-15 11:37:13 +0400

    add calculateExchange method and rounding constants

--- src/com/nik/currencyexchanger/service/ExchangeRateService.java
+++ src/com/nik/currencyexchanger/service/ExchangeRateService.java
@@ -1,18 +1,20 @@
 package com.nik.currencyexchanger.service;
 
 import com.nik.currencyexchanger.dao.ExchangeRateDao;
-import com.nik.currencyexchanger.dto.CurrencyDto;
+import com.nik.currencyexchanger.dto.ExchangeDto;
 import com.nik.currencyexchanger.dto.ExchangeRateDto;
 import com.nik.currencyexchanger.entity.ExchangeRate;
 import com.nik.currencyexchanger.exception.ExchangeRateNotFoundException;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class ExchangeRateService {
 
+    private static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_UP;
+    private static final int DECIMAL_SCALE = 6;
     private static final ExchangeRateService INSTANCE = new ExchangeRateService();
     private final ExchangeRateDao exchangeRateDao = ExchangeRateDao.getInstance();
     private final CurrencyService currencyService = CurrencyService.getInstance();
@@ -43,6 +45,38 @@ public class ExchangeRateService {
         return buildDto(exchangeRate);
     }
 
+    public ExchangeDto calculateExchange(String baseCode, String targetCode, BigDecimal amount){
+        var baseCurrencyId = currencyService.getCurrencyByCode(baseCode).getId();
+        var targetCurrencyId = currencyService.getCurrencyByCode(targetCode).getId();
+        var exchangeRateOptional = exchangeRateDao.findByCurrenciesId(baseCurrencyId, targetCurrencyId);
+
+        if(exchangeRateOptional.isPresent()){
+            var exchangeRate = exchangeRateOptional.get();
+            var rate = exchangeRate.getRate();
+            var convertedAmount = rate.multiply(amount).setScale(2, ROUNDING_MODE);
+            return buildDto(exchangeRate, amount, convertedAmount);
+        }
+        else{
+            var usdId = currencyService.getCurrencyByCode("USD").getId();
+            var usdToBase = exchangeRateDao.findByCurrenciesId(usdId, baseCurrencyId)
+                    .orElseThrow(() -> new ExchangeRateNotFoundException());
+            var usdToTarget = exchangeRateDao.findByCurrenciesId(usdId, targetCurrencyId)
+                    .orElseThrow(() -> new ExchangeRateNotFoundException());
+            var usdToTargetRate = usdToTarget.getRate();
+            var usdToBaseRate = usdToBase.getRate();
+
+            BigDecimal newRate = usdToTargetRate.divide(usdToBaseRate, DECIMAL_SCALE, ROUNDING_MODE);
+            var baseToTargetRate = new ExchangeRate(
+                    0,
+                    baseCurrencyId,
+                    targetCurrencyId,
+                    newRate
+            );
+            var convertedAmount = newRate.multiply(amount).setScale(2, ROUNDING_MODE);
+            return buildDto(baseToTargetRate, amount, convertedAmount);
+        }
+    }
+
     public ExchangeRateDto createExchangeRate(String baseCode, String targetCode, BigDecimal rate){
         var baseId = currencyService.getCurrencyByCode(baseCode).getId();
         var targetId = currencyService.getCurrencyByCode(targetCode).getId();
@@ -65,6 +99,16 @@ public class ExchangeRateService {
                 currencyService.getCurrencyById(exchangeRate.getBaseCurrencyId()),
                 currencyService.getCurrencyById(exchangeRate.getTargetCurrencyId()),
                 exchangeRate.getRate()
+        );
+    }
+
+    private ExchangeDto buildDto(ExchangeRate exchangeRate, BigDecimal amount, BigDecimal convertedAmount){
+        return new ExchangeDto(
+                currencyService.getCurrencyById(exchangeRate.getBaseCurrencyId()),
+                currencyService.getCurrencyById(exchangeRate.getTargetCurrencyId()),
+                exchangeRate.getRate(),
+                amount,
+                convertedAmount
         );
     }
 
