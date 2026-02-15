4285b75dd74d5c77d7ff2c6b8ca7163e2ae7ddfd
dab1231 <nikita11200701@gmail.com>
2026-02-15 12:58:37 +0400

    add doPost method to create currencies

--- src/com/nik/currencyexchanger/servlet/CurrenciesServlet.java
+++ src/com/nik/currencyexchanger/servlet/CurrenciesServlet.java
@@ -1,10 +1,13 @@
 package com.nik.currencyexchanger.servlet;
 
 import com.google.gson.Gson;
+import com.nik.currencyexchanger.dto.CurrencyDto;
+import com.nik.currencyexchanger.exception.CurrencyAlreadyExistsException;
 import com.nik.currencyexchanger.exception.DataBaseException;
 import com.nik.currencyexchanger.service.CurrencyService;
 import com.nik.currencyexchanger.util.ErrorSetter;
 import com.nik.currencyexchanger.util.StatusSetter;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -34,4 +37,30 @@ public class CurrenciesServlet extends HttpServlet {
         }
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            var name = req.getParameter("name");
+            var code = req.getParameter("code");
+            var sign = req.getParameter("sign");
+
+            if(name == null || code == null || sign == null
+                    || name.isBlank() || code.isBlank() || sign.isBlank()){
+                ErrorSetter.setError(resp, 400, "The required form field is missing.");
+                return;
+            }
+
+            var currency = currencyService.createCurrency(name, code, sign);
+            StatusSetter.setHeadersAndStatus(resp, 201);
+            var jsonString = gson.toJson(currency);
+            resp.getWriter()
+                    .write(jsonString);
+        }
+        catch (CurrencyAlreadyExistsException e) {
+            ErrorSetter.setError(resp, 409, "Curency with this code already exists");
+        }
+        catch (DataBaseException e){
+            ErrorSetter.setError(resp, 500, "DB error");
+        }
+    }
 }
