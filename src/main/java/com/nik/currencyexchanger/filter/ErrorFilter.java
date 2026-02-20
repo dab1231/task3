2cb766ed5b55fdb64bc83e7947418322badfdb6c
dab1231 <nikita11200701@gmail.com>
2026-02-21 00:04:50 +0400

    refactor: inline error handling utilities into filter

--- src/main/java/com/nik/currencyexchanger/filter/ErrorFilter.java
+++ src/main/java/com/nik/currencyexchanger/filter/ErrorFilter.java
@@ -1,38 +1,52 @@
 package com.nik.currencyexchanger.filter;
 
+import com.google.gson.Gson;
 import com.nik.currencyexchanger.exception.*;
-import com.nik.currencyexchanger.util.ErrorSetter;
 import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 @WebFilter("/*")
 public class ErrorFilter implements Filter {
 
+    private final Gson gson = new Gson();
+
+    private void sendError(HttpServletResponse response, int status, String message) throws IOException {
+        response.setStatus(status);
+        response.setContentType("application/json");
+        response.setCharacterEncoding(StandardCharsets.UTF_8);
+        String errorJson = gson.toJson(Map.of("message", message));
+        response.getWriter()
+                .write(errorJson);
+    }
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        var httpResp = (HttpServletResponse) servletResponse;
         try{
             filterChain.doFilter(servletRequest, servletResponse);
         }
         catch (DataBaseException e) {
-            ErrorSetter.setError(((HttpServletResponse) servletResponse), 500, "DB error");
+            sendError( httpResp, 500, "DB error");
         }
         catch (CurrencyNotFoundException e){
-            ErrorSetter.setError(((HttpServletResponse) servletResponse),404, "Currency not found");
+            sendError(httpResp,404, "Currency not found");
         }
         catch (CurrencyAlreadyExistsException e) {
-            ErrorSetter.setError(((HttpServletResponse) servletResponse), 409, "Currency with this code already exists");
+            sendError(httpResp, 409, "Currency with this code already exists");
         }
         catch (NumberFormatException e) {
-            ErrorSetter.setError(((HttpServletResponse) servletResponse), 400, "Invalid rate format");
+            sendError(httpResp, 400, "Invalid rate format");
         }
         catch (ExchangeRateAlreadyExistsException e) {
-            ErrorSetter.setError(((HttpServletResponse) servletResponse), 409, "Currency pair with this code already exists. ");
+            sendError(httpResp, 409, "Currency pair with this code already exists. ");
         }
         catch (ExchangeRateNotFoundException e){
-            ErrorSetter.setError(((HttpServletResponse) servletResponse), 404, e.getMessage());
+            sendError(httpResp, 404, e.getMessage());
         }
     }
 }
