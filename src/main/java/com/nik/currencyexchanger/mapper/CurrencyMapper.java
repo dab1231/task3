409d1baf4abd86e05e6cad658c76e898ea5cb986
dab1231 <nikita11200701@gmail.com>
2026-02-23 20:46:19 +0400

    add CurrencyMapper

--- src/main/java/com/nik/currencyexchanger/mapper/CurrencyMapper.java
+++ src/main/java/com/nik/currencyexchanger/mapper/CurrencyMapper.java
@@ -1,6 +1,12 @@
 package com.nik.currencyexchanger.mapper;
+import com.nik.currencyexchanger.dto.response.CurrencyResponseDto;
+import com.nik.currencyexchanger.entity.Currency;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-
+@Mapper
 public interface CurrencyMapper {
 
+    @Mapping(source = "fullName", target = "name")
+    CurrencyResponseDto toDto(Currency currency);
 }
